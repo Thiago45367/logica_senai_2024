bacc56f2033aa287eb8e281bdbba29c802b43cfb
Thiago45367 <161034897+Thiago45367@users.noreply.github.com>
2024-03-15 10:10:58 -0300

    Add files via upload

---
+++ Atividade 1 de C.c
@@ -0,0 +1,21 @@
+#include <stdio.h> //Texto obrigatório em qualquer programação.
+
+// std -> standard=padrão
+// i -> input=entrada
+// o -> output=saída
+
+// Função principal.
+int main(){   // Main=Inicio(PORTUGOL)
+
+	// Comando para mostrar no terminal.
+	printf("hello, world!");   // Printf=Escreva(PORTUGOL)
+	
+	// Comando para pressionar uma tecla
+	// antes de finalizar o programa.
+	getchar();  
+	
+	// Comando para retornar o código 0.
+	// Código usado para informar que o
+	// programa finalizou corretamente.
+	return 0; 
+}
