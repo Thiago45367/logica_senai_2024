b0ac3dc57b919621eba422384c2fd0c926b1b524
Thiago45367 <161034897+Thiago45367@users.noreply.github.com>
2024-03-25 08:05:02 -0300

    Add files via upload

---
+++ Exemplo de repetição.c
@@ -0,0 +1,18 @@
+// Algoritmo: Números de 1 a 10 
+
+int main() {
+	int i;
+	for (i = 1; i <= 10; i++) {
+		printf("%d \n", i);
+	}
+// Algoritmo: Números de 10 a 1.	
+int main() {
+	int j;
+	for (j = 10; j >=1; j--) {
+		printf("%d \n", j);
+	}
+}
+	
+	
+return 0;	
+}
