da7d100062d982bbcfcf9585b97a8435b3c53cfd
Thiago45367 <161034897+Thiago45367@users.noreply.github.com>
2024-04-09 08:39:45 -0300

    Add files via upload

---
+++ Exemplos de vetores e reptição C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <limits.h>
+
+
+int main() {
+	setlocale(LC_ALL,"");
+	
+	int vetor[5];
+	int i;
+	
+	
+	printf("Escreva os elementos do vetor:\n");
+	for (i = 0; i < 5; i++) {
+		printf("Elemento %d: ", i + 1);
+		scanf("%d", &vetor[i]);
+	}
+	
+	
+	
+return 0;	
+}
