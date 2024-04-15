3f50eeca2207526018a49c7dcacdf4c0c33390c9
Thiago45367 <161034897+Thiago45367@users.noreply.github.com>
2024-04-15 09:13:21 -0300

    Add files via upload

--- Sem Título3.c
+++ Sem Título3.c
@@ -1,37 +1,75 @@
-# include <stdio.h>
-# include <locale.h>
-# include <string.h>
-
-#define TAM 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+#include <limits.h>
 
 int main() {
-	setlocale(LC_ALL, "Portuguese");
-    
-    // variaveis.
-    float numero[TAM], numeroPositivo = 0;
-    int i, numeroNegativo = 0;
-    
-    // dados do usuário.
-    printf("Escreva um número:\n");
-    for (i = 0; i < TAM; i++) {
-   		printf("Escreva o %iº número:", i+1);
-   		scanf("%f",&numero[i]);
-   		
-   		if (numero[i] < 0) {
-   				numeroNegativo++;
-		   } else if (numero[i] > 0) {
-		   		numeroPositivo += numero[i];
-		   }
-	}
-	
-	printf("\n=== Exibindo dados para o usuário === \n");
-	for (i = 0; i < TAM; i++) {
-			printf("%iº número: %.1f \n", i+1, numero[i]);
-	}
-	
-	printf("\nQuantidade de números negativos: %i \n", numeroNegativo);
-	printf("soma de números positivos: %.1f \n", numeroPositivo);
-
-
-return 0;
+    setlocale(LC_ALL, "");
+   
+    int opcao, quantidadeFamilias = 0, quantidadeSalarios = 0;
+    int filhos, quantidadeFilhos = 0, somaFilhos = 0;
+    float salario, somaSalario = 0, mediaSalario, mediaFilhos;
+    float maiorSalario = INT_MIN, menorSalario = INT_MAX;
+   
+    do {
+        system("cls || clear");
+        printf("========== MENU ==========\n");
+        printf("1 - Adicionar família \n");
+        printf("2 - Sair e exibir resultados \n");
+        printf("Digite a opção desejada: ");
+        scanf("%i",&opcao);
+       
+        switch(opcao) {
+            case 1:
+                system("cls || clear");
+                printf("========== PESQUISA ==========\n");
+               
+                printf("Digite a quantidade de filhos: ");
+                scanf("%i",&filhos);
+               
+                printf("Digite o salário da família: ");
+                scanf("%f",&salario);
+               
+                quantidadeFamilias++;
+                quantidadeSalarios++;
+                quantidadeFilhos++;
+               
+                somaSalario += salario;
+                somaFilhos += filhos;
+               
+                if (salario > maiorSalario) {
+                    maiorSalario = salario;
+                }
+               
+                if (salario < menorSalario) {
+                    menorSalario = salario;
+                }
+               
+                break;
+            case 2:
+                break;
+            default:
+                printf("Opção inválida. \n");
+                sleep(4);
+                system("cls || clear");
+        }
+    } while (opcao != 2);
+   
+    if (quantidadeFamilias == 0) {
+        printf("\nNão foram informados dados necessários.");
+    } else {
+        mediaSalario = somaSalario / (float) quantidadeSalarios;
+        mediaFilhos = somaFilhos / (float) quantidadeFilhos;
+       
+        system("cls || clear");
+        printf("\n=== Exibindo resultados ===\n");
+        printf("Quantidade de famílias: %i \n", quantidadeFamilias);
+        printf("Média salarial: R$ %.2f \n", mediaSalario);
+        printf("Média de filhos: %.1f \n", mediaFilhos);
+        printf("Maior salário: R$ %.2f \n", maiorSalario);
+        printf("Menor salário: R$ %.2f \n", menorSalario);
+    }
+   
+    return 0;
 }
