1172d1d555ad0d37d71ba698c111ecfee871d853
Thiago45367 <161034897+Thiago45367@users.noreply.github.com>
2024-04-15 09:09:45 -0300

    Add files via upload

---
+++ Sem Título1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+#define TAM 5
+
+int main() {
+	setlocale(LC_ALL,"");
+	// variaveis.
+	int numero[TAM];
+	int numeroPares = 0, numeroImpares = 0, numeroPositivos = 0, numeroNegativos = 0;
+	
+	
+	// dados do usuário.
+	printf("Escreva cinco números.");
+	
+	int i = 0;
+	
+	for (i = 0; i < TAM; i++) {
+		printf("Escreva o %iº número:",i+1);
+		scanf("%i",&numero[i]);
+		
+		if (numero[i] < 0) {
+				numeroNegativos++;
+		} else if (numero[i] > 0) {
+				numeroPositivos += numero[i];
+		}
+		if (numero[i] % 2 == 0){
+			numeroPares++;
+		} else {
+			numeroImpares++;
+		} 
+		
+	}
+	
+	printf("\n=== Exibindo dados para o usuário ===\n");
+	
+	printf("Número positivo: %i \n", numeroPositivos);
+	printf("Número negativo: %i \n", numeroNegativos);
+	printf("Número pares: %i \n", numeroPares);
+	printf("Número impar: %i \n", numeroImpares);
+	printf("Números inseridos: %i \n", TAM);
+	
+	
+		
+	
+	
+return 0;	
+}
